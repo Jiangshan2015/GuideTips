ffa6a8da3dcb72ca84ff947367470b213c8e9257
Yongheng1992 <2432059718@qq.com>
2016-03-04 16:19:00 +0800

    第一次创建

---
+++ Mytest/Mytest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Mytest
+//
+//  Created by songge on 16/3/4.
+//  Copyright © 2016年 占耀松. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Mytest/Mytest/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Mytest
+//
+//  Created by songge on 16/3/4.
+//  Copyright © 2016年 占耀松. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
